5eac69cb2d3a3b0b4050a0fa9207842bcdfae5e5
Anchit Sinha <anchit1909@gmail.com>
2024-02-28 14:10:00 +0530

    feat: basic server setup

---
+++ backend/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"	
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.shortenURL)
+}
+
+func main() {
+	err:=godotenv.Load()
+	if err!=nil {
+		fmt.Println(err)
+	}
+
+	app:=fiber.New()
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
