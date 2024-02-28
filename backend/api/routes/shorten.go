453b5c84e9d07801eaf4fbfef5b6c83762b17ff9
Anchit Sinha <anchit1909@gmail.com>
2024-02-28 14:34:04 +0530

    feat: validate request from user

--- backend/api/routes/shorten.go
+++ backend/api/routes/shorten.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 type request struct {
 	URL		string 		`json:"url"`
@@ -14,4 +16,27 @@ type response struct {
 	Expiry		time.Duration `json:"expiry"`
 	XRateRemaining		int		`json:"rate_limit"`
 	XRateLimitReset		time.Duration		`json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err:= c.BodyParser(&body); err!=nil {
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map {"error":"cannot parse JSON"})
+ }
+
+ //implement rate limiting
+
+
+ //check if the imput is an actual URL
+ if !govalidator.IsURL(body.URL) {
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map {"error":"Invalid URL"})
+ }
+
+ //check for domain error
+ if !helpers.RemoveDomainError(body.URL) {
+	return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map {"error":"You cannot access the system"})
+ }
+
+ //enforce https, SSL
+ body.URL = helpers.EnforceHTTP(body.URL)
 }
