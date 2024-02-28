e79df6d7f5d113862594ff5468ac21ce1dbba555
Anchit Sinha <anchit1909@gmail.com>
2024-02-28 18:13:06 +0530

    feat: implement shorten route

--- backend/api/routes/shorten.go
+++ backend/api/routes/shorten.go
@@ -10,6 +10,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 type request struct {
 	URL		string 		`json:"url"`
@@ -61,4 +62,48 @@ func ShortenURL(c *fiber.Ctx) error {
 
  //enforce https, SSL
  body.URL = helpers.EnforceHTTP(body.URL)
+
+ var id string
+ if body.CustomShort == "" {
+	id = uuid.New().String()[:6]
+ } else {
+	id = body.CustomShort
+ }
+
+ r := database.CreateClient(0);
+ defer r.Close()
+
+ val, _ = r.Get(database.Ctx, id).Result()
+ if val != "" {
+	return c.Status(fiber.StatusForbidden).JSON(fiber.Map {"error":"URL custom short is already in use",})
+ }
+
+ if body.Expiry == 0 {
+	body.Expiry = 24
+ }
+
+ err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+ if err!=nil {
+	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {"error":"Unable to connect to server"})
+ }
+
+ resp := response {
+	URL:		body.URL,
+	CustomShort:		"",
+	Expiry:		body.Expiry,
+	XRateRemaining: 10,
+	XRateLimitReset: 30,
+ }
+
+ r2.Decr(database.Ctx, c.IP())
+
+ val, _ = r2.Get(database.Ctx, c.IP()).Result()
+ resp.XRateRemaining, _ = strconv.Atoi(val)
+
+ ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+ resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+ resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+ return c.Status(fiber.StatusOK).JSON(resp)
 }
