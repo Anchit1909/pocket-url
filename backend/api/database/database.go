f63caa1b921ecb00c63a309cac0b4237c0a96ce5
Anchit Sinha <anchit1909@gmail.com>
2024-03-05 15:32:05 +0530

    chore: removed DB password

--- backend/api/database/database.go
+++ backend/api/database/database.go
@@ -12,7 +12,7 @@ var Ctx = context.Background()
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options {
 		Addr: os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASS"),
+		Password: os.Getenv(""),
 		DB: dbNo,
 	})
 	return rdb
