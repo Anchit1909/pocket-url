8524099430f94a6b50f02da31da5fc5ce47f54de
Anchit Sinha <anchit1909@gmail.com>
2024-03-05 15:32:16 +0530

    chore: changed backend URL

--- frontend/src/components/InputBox/InputBox.tsx
+++ frontend/src/components/InputBox/InputBox.tsx
@@ -38,7 +38,7 @@ const InputBox = ({
   const [shortLink, setShortLink] = React.useState("");
   const shortenLink = async () => {
     try {
-      const url = "http://localhost:3000/api/v1/";
+      const url = "https://shorten-url-doip.onrender.com/";
       const requestData = {
         url: longURL,
         short: customURL,
