7272a5c9bdbf1aa50d14adb1169445691239e866
Anchit Sinha <anchit1909@gmail.com>
2024-07-08 15:51:21 +0530

    build: update backend prod url

--- frontend/src/components/InputBox/InputBox.tsx
+++ frontend/src/components/InputBox/InputBox.tsx
@@ -38,7 +38,7 @@ const InputBox = ({
   const [shortLink, setShortLink] = React.useState("");
   const shortenLink = async () => {
     try {
-      const url = "http://pocketurl.anchitsinha.xyz:3000/api/v1";
+      const url = "https://pocketurlapi.anchitsinha.xyz/api/v1";
       const requestData = {
         url: longURL,
         short: customURL,
